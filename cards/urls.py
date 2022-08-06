e140430666d5bf7c0da83400ba7b33284758e6e1
bdclauser <pzljug@hotmail.com>
2022-08-05 22:48:36 -0400

    updating html

--- cards/urls.py
+++ cards/urls.py
@@ -6,11 +6,16 @@ urlpatterns = [
     path(
         "",
         views.CardListView.as_view(),
-        name='card-list'
+        name="card-list"
     ),
     path(
         "new",
         views.CardCreateView.as_view(),
         name="card-create"
+    ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
     ),
 ]
