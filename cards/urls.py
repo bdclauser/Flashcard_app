797c580bb333a7343cc2f5c20548c84297ca35d9
bdclauser <pzljug@hotmail.com>
2022-08-05 21:35:43 -0400

    update type in path

--- cards/urls.py
+++ cards/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path(
         "",
-        TemplateView.as_view(template_name='cards/vase.html'),
+        TemplateView.as_view(template_name='cards/base.html'),
         name="home"
     ),
 ]
